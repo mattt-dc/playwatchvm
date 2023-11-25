b805ca129c183ccda0e1febab02ed7f801d23a4a
mattt-dc <matt2021@outlook.com>
2023-11-25 21:16:44 +0000

    Start a world and record a video

--- automation.py
+++ automation.py
@@ -2,6 +2,8 @@ import pyautogui
 import time
 import os
 import subprocess
+import random
+import string
 
 # Path to the shell script
 minetest_binary_path = '/home/vagrant/minetest/bin/minetest'
@@ -23,5 +25,21 @@ pyautogui.press('enter')
 # Wait for Minetest to start
 time.sleep(5)
 
-# Press the down arrow to go to the next menu option
-pyautogui.press('down')
+# Create new world
+pyautogui.moveTo(800,490)
+pyautogui.click()
+random_world_name = ''.join(random.choices(string.ascii_letters, k=10))
+pyautogui.write(random_world_name)
+pyautogui.moveTo(450,600)
+pyautogui.click()
+pyautogui.moveTo(700,520)
+pyautogui.click()
+
+time.sleep(5)
+
+output_file = '/home/vagrant/Videos/' + random_world_name + '.mp4'
+command = f"ffmpeg -y -f x11grab -video_size 957x713 -i :0.0+67,55 -codec:v libx264 {output_file}"
+process = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+time.sleep(3)
+process.terminate()
+process.wait()
--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ sudo apt-get install -y python3-pip
 
 sudo apt-get install -y python3-tk python3-dev
 
+sudo apt-get install -y ffmpeg
+
 (crontab -l 2>/dev/null; echo "@reboot python3 /home/vagrant/automation.py") | crontab -
 
 # Optional: Install additional RetroArch cores
